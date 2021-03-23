3d63fa9ba82f4dd5c972d2480c4b8715ee63ae0c
Aleksandr Serebrovskiy <aleksandr-web@yandex.ru>
2021-03-23 20:21:05 +0300

    mobile-menu

--- pages/scriptMobileMenu.js
+++ pages/scriptMobileMenu.js
@@ -1,14 +1,19 @@
 //откритие/закрытие мобильного меню
 const btnHeader = document.getElementById('header');
-const btnMenu = document.getElementById('menu');
+// const btnMenu = document.getElementById('menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
 function addClass() {
-  mobileMenu.classList.add('mobile-menu_opened');
+  if (mobileMenu.classList.contains('mobile-menu_opened')) {
+    mobileMenu.classList.remove('mobile-menu_opened');
+  } else {
+    mobileMenu.classList.add('mobile-menu_opened');
+  }
+  // mobileMenu.classList.add('mobile-menu_opened');
 }
-function removeClass() {
-  mobileMenu.classList.remove('mobile-menu_opened');
-}
+// function removeClass() {
+//   mobileMenu.classList.remove('mobile-menu_opened');
+// }
 
 btnHeader.addEventListener('click', addClass);
-btnMenu.addEventListener('click', removeClass);
+// btnMenu.addEventListener('click', removeClass);
