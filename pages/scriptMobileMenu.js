a5b89df273354378494060ed15c5f989a0614e73
Aleksandr Serebrovskiy <aleksandr-web@yandex.ru>
2021-03-24 15:20:22 +0300

    mobile menu links active

--- pages/scriptMobileMenu.js
+++ pages/scriptMobileMenu.js
@@ -2,18 +2,29 @@
 const btnHeader = document.getElementById('header');
 // const btnMenu = document.getElementById('menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const mobileLinkList = document.querySelectorAll('.mobile__navigatoin-link-text');
 
-function addClass() {
-  if (mobileMenu.classList.contains('mobile-menu_opened')) {
-    mobileMenu.classList.remove('mobile-menu_opened');
-  } else {
-    mobileMenu.classList.add('mobile-menu_opened');
-  }
-  // mobileMenu.classList.add('mobile-menu_opened');
+
+function toggleClass() {
+  mobileMenu.classList.toggle('mobile-menu_opened');
 }
-// function removeClass() {
-//   mobileMenu.classList.remove('mobile-menu_opened');
-// }
+function removeClass() {
+  mobileMenu.classList.remove('mobile-menu_opened');
+}
 
-btnHeader.addEventListener('click', addClass);
+btnHeader.addEventListener('click', toggleClass);
+for (let link of mobileLinkList) {
+  link.addEventListener('click', removeClass);
+}
+
+
+
+
+  // if (mobileMenu.classList.contains('mobile-menu_opened')) {
+  // mobileMenu.classList.remove('mobile-menu_opened');
+  // }
+  // else {
+  //   mobileMenu.classList.add('mobile-menu_opened');
+  // }
+// btnHeader.addEventListener('click', addClass);
 // btnMenu.addEventListener('click', removeClass);
